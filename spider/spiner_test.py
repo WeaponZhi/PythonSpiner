1faae581694d19dc1c499e854f13f79c8d56b913
WeaponZhi <584098488@qq.com>
2018-01-22 18:19:47 +0800

    爬取博客

--- spider/spiner_test.py
+++ spider/spiner_test.py
@@ -1,41 +1,43 @@
-import re
 import urllib.request
 import urllib
-
+from bs4 import BeautifulSoup
 from collections import deque
 
-queue = deque()
-visited = set()
+queue = deque()  # 待爬取的队列
+visited = set()  # 已爬取的set
 
-url = 'http://weaponzhi.online/'  # 入口页面, 可以换成别的
+host = 'http://weaponzhi.online'  # 入口页面, 可以换成别的
+queue.append(host)
 
-queue.append(url)
-cnt = 0
+count = 0  # 计数器
 
 while queue:
-    if cnt > 100:
-        break
+
     url = queue.popleft()  # 队首元素出队
-    visited |= {url}  # 标记为已访问
+    visited.add(url)  # 标记为已访问
 
-    print('已经抓取: ' + str(cnt) + '   正在抓取 <---  ' + url)
-    cnt += 1
+    print('已经抓取: ' + str(count) + '   正在抓取 <---  ' + url)
+    count += 1
     try:
-        urlop = urllib.request.urlopen(url)
-    except:
-        continue
-    if 'html' not in urlop.getheader('Content-Type'):
-        continue
-
-    # 避免程序异常中止, 用try..catch处理异常
-    try:
-        data = urlop.read().decode('utf-8')
+        url_response = urllib.request.urlopen(url)
+        # 过滤.jpg这种非跳转URL的情况
+        if 'html' not in url_response.getheader('Content-Type'):
+            continue
+        data = url_response.read().decode('utf-8')
     except:
         continue
 
-    # 正则表达式提取页面中所有队列, 并判断是否已经访问过, 然后加入待爬队列
-    linkre = re.compile('href="(.+?)"')
-    for x in linkre.findall(data):
-        if 'http' in x and x not in visited:
-            queue.append(x)
-            print('加入队列 --->  ' + x)
+    soup = BeautifulSoup(data, 'lxml')
+    node = soup.find_all('a')  # 查找a标签节点
+
+    for x in node:
+        try:
+            new_url = x['href']
+            if new_url.startswith('/'):
+                new_url = host + new_url
+            if new_url not in queue and new_url not in visited:
+                if 'http' in new_url and 'weaponzhi' in new_url:
+                    queue.append(new_url)
+                    print('加入队列 ---> ' + new_url)
+        except:
+            continue
